619cd9b3a54dc47315cf93a9ce60a7ccc404a20e
rlaitonh <rlaitonh@hotmail.com>
2021-12-05 01:10:37 -0500

    corrección 2

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static( __dirname + '/dist/frontend'));
+app.use(express.static( __dirname + '/frontend'));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join( __dirname, '/dist/frontend/index.html'));
+    res.sendFile(path.join( __dirname, '/frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
